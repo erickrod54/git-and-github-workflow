98e2361945d174b6b2dfaffe88baa88419029a18
Erick Rodriguez <erickrod54@gmail.com>
2025-01-18 21:35:00 -0500

    git-and-github-wrokflow - version 1.00 - HomePage - Features: Adding HeaderComponent

--- src/pages/home.page.jsx
+++ src/pages/home.page.jsx
@@ -1,9 +1,20 @@
 import React from "react";
+import styled from "styled-components";
+import { HeaderComponent } from "../components/index.components";
+
+
+/**git-and-github-workflow  - version 1.00 - HomePage
+ * - Features: 
+ * 
+ *     --> Adding 'HeaderComponent'    
+ * 
+ * Note: this is the index components
+ */
 
 const HomePage = () => {
 
     return(
-        <h2>git and github workflow</h2>
+        <HeaderComponent />
     )
 }
 
