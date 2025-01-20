496f706a9e5a1a5a77b1e65374915e3e36477771
Erick Rodriguez <erickrod54@gmail.com>
2025-01-19 21:34:00 -0500

    git-andgithub-workflow - version 1.01 - components > index - Features: Importing and exporting FooterComponent

--- src/components/index.components.js
+++ src/components/index.components.js
@@ -1,11 +1,12 @@
+import FooterComponent from "./footer.component";
 import HeaderComponent from "./header.component";
 
-/**git-and-github-workflow  - version 1.00 - components > index
+/**git-and-github-workflow  - version 1.01 - components > index
  * - Features: 
  * 
- *     --> Importing and exporting 'HeaderComponent'    
+ *     --> Importing and exporting 'FooterComponent'    
  * 
  * Note: this is the index components
  */
 
-export { HeaderComponent }
+export { HeaderComponent, FooterComponent }
