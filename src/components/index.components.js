87903494dafb3e0bfb39c1d87c9298b04bafbb14
Erick Rodriguez <erickrod54@gmail.com>
2025-01-19 21:31:00 -0500

    git-and-github-workflow - version 1.00 - components > index - Features: Importing and exporting Header Component

---
+++ src/components/index.components.js
@@ -0,0 +1,11 @@
+import HeaderComponent from "./header.component";
+
+/**git-and-github-workflow  - version 1.00 - components > index
+ * - Features: 
+ * 
+ *     --> Importing and exporting 'HeaderComponent'    
+ * 
+ * Note: this is the index components
+ */
+
+export { HeaderComponent }
