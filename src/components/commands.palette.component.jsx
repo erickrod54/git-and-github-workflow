dd1683e6ea1fe610e6c5e9c00a24fd525cc3a494
Erick Rodriguez <erickrod54@gmail.com>
2025-01-19 21:35:00 -0500

    git-and-github-workflow - version 1.01 - FooterCompoent - Features: Building CommandsPalette

---
+++ src/components/commands.palette.component.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+/**git-and-github-workflow  - version 1.01 - FooterComponent
+ * - Features: 
+ * 
+ *     --> Building 'CoomandsPallete'    
+ * 
+ * Note: this is the index components
+ */
+
+const CoomandsPallete = () => {
+
+    return(
+        <h2>the CoomandsPallete Component</h2>
+    )
+}
+
+export default CoomandsPallete;
