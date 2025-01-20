9e01a575290bedab2637415610d5d871a01885c1
Erick Rodriguez <erickrod54@gmail.com>
2025-01-19 21:33:00 -0500

    git-and-github-wrokflow - version 1.00 - FooterComponent - Features: Building FooterComponent

---
+++ src/components/footer.component.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+/**git-and-github-workflow  - version 1.00 - FooterComponent
+ * - Features: 
+ * 
+ *     --> Building 'FooterComponent'    
+ * 
+ * Note: this is the index components
+ */
+
+const FooterComponent = () => {
+
+    return(
+        <h2>the FooterComponent</h2>
+    )
+}
+
+export default FooterComponent;
