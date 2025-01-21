8d9a5d6561e47c1de4b44797a0681b232ca25191
Erick Rodriguez <erickrod54@gmail.com>
2025-01-20 21:33:00 -0500

    git-and-github-workflow - version 1.03 - assets > index - Features: Importing and exporting clipboard

---
+++ src/assets/index.assets.js
@@ -0,0 +1,11 @@
+import clipboard from './copy.png'
+
+/**git-and-github-workflow  - version 1.03 - assets > index
+ * - Features: 
+ * 
+ *     --> Importing and exporting 'clipboard'   
+ * 
+ * Note: this is the index components
+ */
+
+export { clipboard }
