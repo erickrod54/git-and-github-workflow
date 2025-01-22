0f2f27d3837b9fbd64fd4633035a3d903cb4beb9
Erick Rodriguez <erickrod54@gmail.com>
2025-01-21 21:33:00 -0500

    git-and-github-workflow - version 1.06 - HomePage - Features: Migrating HomePageContainer styles

--- src/pages/home.page.jsx
+++ src/pages/home.page.jsx
@@ -1,23 +1,17 @@
 import React from "react";
 import styled from "styled-components";
 import { CommandsPallete, FooterComponent, HeaderComponent } from "../components/index.components";
+import { HomePageContainer } from "../styled-components/styled.components";
 
 
-/**git-and-github-workflow  - version 1.05 - HomePage
+/**git-and-github-workflow  - version 1.06 - HomePage
  * - Features: 
  * 
- *     --> writting 'MainContent' styles    
+ *     --> Migrating 'HomePageContainer' styles    
  * 
  * Note: this is the index components
  */
 
-const HomePageContainer = styled.div`
-    display: flex;
-    flex-direction: column;
-    min-height: 100vh;
-    background-color: #f3f4f6; /* Light gray background */
-    color: #1f2937; /* Dark gray text */
-`
 
 const MainContent = styled.div`
     flex-grow: 1;
