ec5c1dfcfde2c04b8ff9618f3801c202f3324593
Erick Rodriguez <erickrod54@gmail.com>
2025-01-21 21:30:00 -0500

    git-and-github-workflow - version 1.03 - HomePage - Features: writting HomePageContainer styles

--- src/pages/home.page.jsx
+++ src/pages/home.page.jsx
@@ -3,16 +3,20 @@ import styled from "styled-components";
 import { CommandsPallete, FooterComponent, HeaderComponent } from "../components/index.components";
 
 
-/**git-and-github-workflow  - version 1.03 - HomePage
+/**git-and-github-workflow  - version 1.04 - HomePage
  * - Features: 
  * 
- *     --> Adding site strcuture    
+ *     --> writting 'HomePageContainer' styles    
  * 
  * Note: this is the index components
  */
 
 const HomePageContainer = styled.div`
-
+    display: flex;
+    flex-direction: column;
+    min-height: 100vh;
+    background-color: #f3f4f6; /* Light gray background */
+    color: #1f2937; /* Dark gray text */
 `
 
 const HomePage = () => {
