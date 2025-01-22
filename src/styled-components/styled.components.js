ba4411e772862097031f0dab762a36ea71b3e2ce
Erick Rodriguez <erickrod54@gmail.com>
2025-01-21 21:31:00 -0500

    git-and-github-workflow - version 1.06 - HomePage - Features: writting HomePageContainer styles

---
+++ src/styled-components/styled.components.js
@@ -0,0 +1,17 @@
+import styled from "styled-components"
+
+/**git-and-github-workflow  - version 1.06 - HomePage
+ * - Features: 
+ * 
+ *     --> writting 'HomePageContainer' styles    
+ * 
+ * Note: this is the index components
+ */
+
+export const HomePageContainer = styled.div`
+    display: flex;
+    flex-direction: column;
+    min-height: 100vh;
+    background-color: #f3f4f6; /* Light gray background */
+    color: #1f2937; /* Dark gray text */
+`
