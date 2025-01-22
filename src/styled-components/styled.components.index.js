fccc5ef109bea3e91e86e6823cda53b4263f2ff4
Erick Rodriguez <erickrod54@gmail.com>
2025-01-21 21:32:00 -0500

    git-and-github-workflow - version 1.06 - styled.components > index - Features: Importing and exporting HomePageContainer styles

---
+++ src/styled-components/styled.components.index.js
@@ -0,0 +1,11 @@
+import { HomePageContainer } from "./styled.components";
+
+/**git-and-github-workflow  - version 1.06 - styled.components > index
+ * - Features: 
+ * 
+ *     --> importing and Exporting 'HomePageContainer' styles    
+ * 
+ * Note: this is the index components
+ */
+
+export { HomePageContainer }
