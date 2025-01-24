4a536a51211d6779721ac980cbc5792c58f9b123
Erick Rodriguez <erickrod54@gmail.com>
2025-01-23 21:34:00 -0500

    git-and-github-workflow - version 1.12 - data js - Features: Adding TitlesData

---
+++ src/data.js
@@ -0,0 +1,23 @@
+/**git-and-github-workflow  - version 1.12 - data js
+ * - Features: 
+ *    
+ *     --> Adding 'TitlesData'
+ * 
+ * Note: This component will have later the main menu
+ * to each pattern and its explanations and use cases
+ */
+
+export const TitlesData = [
+    {
+      id:1,
+      category:'Main Title',
+      pattern:'Welcome to the Git and GitHub Workflow guide!',
+      description:''
+    },
+    {
+      id:2,
+      category:'sub titles',
+      pattern:'here is the next sub title',
+      description:''
+    },
+]
