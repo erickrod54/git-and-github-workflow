8807cc01d0f8bb47b8b52af2d4ab7e3f447987a8
Erick Rodriguez <erickrod54@gmail.com>
2025-01-23 21:32:00 -0500

    git-and-github-workflow - version 1.11 - - HomePage - Features: drilling props to HeaderComponent

--- src/pages/home.page.jsx
+++ src/pages/home.page.jsx
@@ -4,10 +4,10 @@ import { CommandsPallete, FooterComponent, HeaderComponent } from "../components
 import { GlobalStyles, HomePageContainer, MainContent } from "../styled-components/styled.components";
 
 
-/**git-and-github-workflow  - version 1.09 - HomePage
+/**git-and-github-workflow  - version 1.11 - HomePage
  * - Features: 
  * 
- *     --> implementing 'GlobalStyles'     
+ *     --> drilling props to 'HeaderComponent'     
  * 
  * Note: this is the index components
  */
@@ -21,7 +21,7 @@ const HomePage = () => {
         <GlobalStyles />
         <HomePageContainer>
             <MainContent>
-                <HeaderComponent />
+                <HeaderComponent title={'Welcome to the Git and GitHub Workflow guide!'}/>
                 <CommandsPallete />
                 <FooterComponent />
             </MainContent>
