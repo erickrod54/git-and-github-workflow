a1b53aa92189087a54488f8a443c0ad75b7be499
Erick Rodriguez <erickrod54@gmail.com>
2025-01-24 21:32:00 -0500

    git-and-github-workflow - version 1.15 - data js - Features: Editing TitlesData fields

--- src/data.js
+++ src/data.js
@@ -1,7 +1,7 @@
-/**git-and-github-workflow  - version 1.12 - data js
+/**git-and-github-workflow  - version 1.15 - data js
  * - Features: 
  *    
- *     --> Adding 'TitlesData'
+ *     --> Editing 'TitlesData' fields
  * 
  * Note: This component will have later the main menu
  * to each pattern and its explanations and use cases
@@ -11,13 +11,13 @@ export const TitlesData = [
     {
       id:1,
       category:'Main Title',
-      pattern:'Welcome to the Git and GitHub Workflow guide!',
+      title:'Welcome to the Git and GitHub Workflow guide!',
       description:''
     },
     {
       id:2,
       category:'sub titles',
-      pattern:'here is the next sub title',
+      title:'here is the next sub title',
       description:''
     },
 ]
