4532677f9f0445873f4b3664c1c215dc6b4ba667
Erick Rodriguez <erickrod54@gmail.com>
2025-01-24 21:35:00 -0500

    git-and-github-workflow - version 1.15 - App - Features: Adding AppProvider

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
 import React from "react";
 import HomePage from "./pages/home.page";
+import { AppProvider } from "./context";
+
+/**git-and-github-workflow  - version 1.15 - App
+ * - Features: 
+ * 
+ *     --> Adding AppProvider    
+ * 
+ * Note: this is the index components
+ */
+
 
 function App() {
   return (
     <>
+    <AppProvider>
       <HomePage />
+    </AppProvider>
     </>
   );
 }
