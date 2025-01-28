af45de79ed1773ed942004a58d586b3f887d3a80
Erick Rodriguez <erickrod54@gmail.com>
2025-01-27 21:35:00 -0500

    git-and-github-workflow - version 2.04 - SingleCardCommnad - Features: Building SingleCardComponent

---
+++ src/components/single.command.card.component.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import styled from "styled-components";
+
+/**git-and-github-workflow  - version 2.04 - SingleCardCommand
+ * - Features: 
+ * 
+ *     --> Building  'SingleCardComponent'
+ * 
+ * Note: this is the index components
+ */
+
+
+const SingleCardComponent = () => {
+
+    return(
+    <div class="container">
+        <div class="box">
+            <span class="title">GLASS EFFECT</span>
+            <div>
+            <strong>SingleCardComponent</strong>
+            <p>0000 000 000 0000</p>
+            <span>VALID</span> <span>01/28</span>
+            </div>
+        </div>
+    </div>
+    )
+}
+
+export default SingleCardComponent
