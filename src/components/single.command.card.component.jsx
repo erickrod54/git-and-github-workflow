819067484d5a576c57a32b81c217d010db1caeb4
Erick Rodriguez <erickrod54@gmail.com>
2025-01-28 21:33:00 -0500

    git-and-github-workflow - version 2.06 - SingleCardCommand - Features: writting CardContainer before and after effects

--- src/components/single.command.card.component.jsx
+++ src/components/single.command.card.component.jsx
@@ -1,24 +1,45 @@
 import React from "react";
 import styled from "styled-components";
 
-/**git-and-github-workflow  - version 2.05 - SingleCardCommand
+/**git-and-github-workflow  - version 2.06 - SingleCardCommand
  * - Features: 
  * 
- *     --> writting  'CardContainer'
+ *     --> writting  'CardContainer' before and after effetcs
  * 
  * Note: this is the index components
  */
 
 const CardContainer = styled.div`
-    color: white;
+    color: black;
     position: relative;
     font-family: sans-serif;
+
+    &::before,
+    &::after {
+        content: "";
+        background-color: #fab5704c;
+        position: absolute;
+        width: 100%;  /* Adjust width if needed */
+        height: 100%; /* Adjust height if needed */
+        top: 0;
+        left: 0;
+        z-index: -1; /* Ensures it stays behind the content */
+    }
+
+    &::after {
+        content: "";
+        position: absolute;
+        height: 3rem;
+        top: 8%;
+        right: 5%;
+        border: 1px solid;
+    }
 `
 
 const SingleCardComponent = () => {
 
     return(
-    <div className="container">
+    <CardContainer>
         <div className="box">
             <span className="title">GLASS EFFECT</span>
             <div>
@@ -27,7 +48,7 @@ const SingleCardComponent = () => {
             <span>VALID</span> <span>01/28</span>
             </div>
         </div>
-    </div>
+    </CardContainer>
     )
 }
 
