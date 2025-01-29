373550899d3432caf30c088eee546827c0660b03
Erick Rodriguez <erickrod54@gmail.com>
2025-01-28 21:30:00 -0500

    git-and-github-workflow - version 2.05 - SingleCardCommand - Features: writting CardContainer

--- src/components/single.command.card.component.jsx
+++ src/components/single.command.card.component.jsx
@@ -1,21 +1,26 @@
 import React from "react";
 import styled from "styled-components";
 
-/**git-and-github-workflow  - version 2.04 - SingleCardCommand
+/**git-and-github-workflow  - version 2.05 - SingleCardCommand
  * - Features: 
  * 
- *     --> Building  'SingleCardComponent'
+ *     --> writting  'CardContainer'
  * 
  * Note: this is the index components
  */
 
+const CardContainer = styled.div`
+    color: white;
+    position: relative;
+    font-family: sans-serif;
+`
 
 const SingleCardComponent = () => {
 
     return(
-    <div class="container">
-        <div class="box">
-            <span class="title">GLASS EFFECT</span>
+    <div className="container">
+        <div className="box">
+            <span className="title">GLASS EFFECT</span>
             <div>
             <strong>SingleCardComponent</strong>
             <p>0000 000 000 0000</p>
