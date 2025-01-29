56e5744babc2298c13b119f954ffc8b3f62a729a
Erick Rodriguez <erickrod54@gmail.com>
2025-01-28 21:34:00 -0500

    git-and-github-workflow - version 2.07 - SingleCardCommand - Features: writting BoxContainer styles

--- src/components/single.command.card.component.jsx
+++ src/components/single.command.card.component.jsx
@@ -1,53 +1,74 @@
 import React from "react";
 import styled from "styled-components";
 
-/**git-and-github-workflow  - version 2.06 - SingleCardCommand
+/**git-and-github-workflow  - version 2.07- SingleCardCommand
  * - Features: 
  * 
- *     --> writting  'CardContainer' before and after effetcs
+ *     --> writting  'BoxContainer' styles
  * 
  * Note: this is the index components
  */
 
-const CardContainer = styled.div`
-    color: black;
-    position: relative;
-    font-family: sans-serif;
+export const CardContainer = styled.div`
+  color: black;
+  position: relative;
+  font-family: sans-serif;
 
-    &::before,
-    &::after {
-        content: "";
-        background-color: #fab5704c;
-        position: absolute;
-        width: 100%;  /* Adjust width if needed */
-        height: 100%; /* Adjust height if needed */
-        top: 0;
-        left: 0;
-        z-index: -1; /* Ensures it stays behind the content */
-    }
+  &::before,
+  &::after {
+    content: "";
+    background-color: #fab5704c;
+    position: absolute;
+  }
 
-    &::after {
-        content: "";
-        position: absolute;
-        height: 3rem;
-        top: 8%;
-        right: 5%;
-        border: 1px solid;
-    }
-`
+  &::before {
+    border-radius: 50%;
+    width: 6rem;
+    height: 6rem;
+    top: 30%;
+    right: 7%;
+  }
+
+  &::after {
+    height: 3rem;
+    top: 8%;
+    right: 5%;
+    border: 1px solid;
+  }
+`;
+
+export const BoxContainer = styled.div`
+  width: fit-content;
+  height: 15.875em;
+  padding: 1rem;
+  background-color: rgba(255, 255, 255, 0.074);
+  border: 1px solid rgba(255, 255, 255, 0.222);
+  -webkit-backdrop-filter: blur(20px);
+  backdrop-filter: blur(20px);
+  border-radius: 0.7rem;
+  transition: all ease 0.3s;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+
+  &:hover {
+    box-shadow: 0px 0px 20px 1px #ffbb763f;
+    border: 1px solid rgba(255, 255, 255, 0.454);
+  }
+`;
 
 const SingleCardComponent = () => {
 
     return(
     <CardContainer>
-        <div className="box">
+        <BoxContainer>
             <span className="title">GLASS EFFECT</span>
             <div>
             <strong>SingleCardComponent</strong>
             <p>0000 000 000 0000</p>
             <span>VALID</span> <span>01/28</span>
             </div>
-        </div>
+        </BoxContainer>
     </CardContainer>
     )
 }
