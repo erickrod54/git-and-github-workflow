ef003679831b55890f59c204afd515072b4a8c6c
Erick Rodriguez <erickrod54@gmail.com>
2025-01-28 21:31:00 -0500

    git-and-github-workflow - version 2.05 - components > index - Features: Importing and exporting SingleCardComponent

--- src/components/index.components.js
+++ src/components/index.components.js
@@ -1,14 +1,15 @@
 import CommandsPallete from "./commands.palette.component";
 import FooterComponent from "./footer.component";
 import HeaderComponent from "./header.component";
+import SingleCardComponent from "./single.command.card.component";
 import SingleCommand from "./single.command.component";
 
-/**git-and-github-workflow  - version 2.04 - components > index
+/**git-and-github-workflow  - version 2.05 - components > index
  * - Features: 
  * 
- *     --> Importing and exporting 'SingleCommand'    
+ *     --> Importing and exporting 'SingleCardComponent'    
  * 
  * Note: this is the index components
  */
 
-export { HeaderComponent, FooterComponent, CommandsPallete, SingleCommand }
+export { HeaderComponent, FooterComponent, CommandsPallete, SingleCommand, SingleCardComponent }
