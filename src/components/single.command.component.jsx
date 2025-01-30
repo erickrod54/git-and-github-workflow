67db79a5dd0365002fe6caf8f5caf97ed8d377e3
Erick Rodriguez <erickrod54@gmail.com>
2025-01-29 21:33:00 -0500

    git-and-github-workflow - version 2.10 - SingleCommand - Features: Adding git status first command test

--- src/components/single.command.component.jsx
+++ src/components/single.command.component.jsx
@@ -2,23 +2,22 @@ import React from "react"
 import BashCode from "./bash.code.component"
 import { useGitGuideContext } from "../context"
 
-/**git-and-github-workflow  - version 2.04 - SingleCommand
+/**git-and-github-workflow  - version 2.10 - SingleCommand
  * - Features: 
  * 
- *     --> Building  'SingleCommand' 
- * 
- *     --> Testing 'gitCommands'
+ *     --> Adding 'git status' first command test
  * 
  * Note: this is the index components
  */
 
 const SingleCommand = () => {
     
-    const { gitCommands } = useGitGuideContext()
+    const { gitCommands } = useGitGuideContext();
 
-    console.log('this is the gitCommands data ==>', gitCommands)
+    const gitstatus = gitCommands[0].command;
+    
     return(
-        <h2>this is the single command component</h2>
+        <BashCode code={gitstatus}/>
     )
 }
 
