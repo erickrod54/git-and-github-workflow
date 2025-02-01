1377ecbe925819594d8acb4ce4e844ec67e6f6e3
Erick Rodriguez <erickrod54@gmail.com>
2025-01-31 21:33:00 -0500

    git-and-github-workflow - version 2.15 - CoomandPallete - Features: writting CommandPalleteContainer

--- src/components/commands.palette.component.jsx
+++ src/components/commands.palette.component.jsx
@@ -1,21 +1,26 @@
 import React from "react";
 import SingleCardComponent from "./single.command.card.component";
+import styled from "styled-components";
 
-/**git-and-github-workflow  - version 2.05 - CommandsPallete
+/**git-and-github-workflow  - version 2.15 - CommandsPallete
  * - Features: 
  * 
- *     --> Adding 'SingleCardComponent'  
+ *     --> writting 'CommandsPalleteContainer'  
  * 
  * Note: this is the index components
  */
 
+const CommandsPalleteContainer = styled.div`
+
+`
+
 const CommandsPallete = () => {
 
     return(
-        <>
+        <CommandsPalleteContainer>
             <h2>the CoomandsPallete Component</h2>
             <SingleCardComponent />
-        </>
+        </CommandsPalleteContainer>
     )
 }
 
