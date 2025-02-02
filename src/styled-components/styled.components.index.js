0711d9fee3e8d6d8f45d6d3f8162b9d7d3622817
Erick Rodriguez <erickrod54@gmail.com>
2025-02-01 21:35:00 -0500

    git-and-github-workflow - version 2.19 - styled.components > index - Features: Importing and Exporting CommnadsPalleteContainer styles

--- src/styled-components/styled.components.index.js
+++ src/styled-components/styled.components.index.js
@@ -1,11 +1,11 @@
-import { HomePageContainer, MainContent, MainTitle, SubTitle } from "./styled.components";
+import { CommandsPalleteContainer, HomePageContainer, MainContent, MainTitle, SubTitle } from "./styled.components";
 
-/**git-and-github-workflow  - version 2.17 - styled.components > index
+/**git-and-github-workflow  - version 2.19 - styled.components > index
  * - Features: 
  * 
- *     --> importing and Exporting 'SubTitle' styles    
+ *     --> importing and Exporting 'CommandsPalleteContainer' styles    
  * 
  * Note: this is the index components
  */
 
-export { HomePageContainer, MainContent, MainTitle, SubTitle }
+export { HomePageContainer, MainContent, MainTitle, SubTitle, CommandsPalleteContainer }
