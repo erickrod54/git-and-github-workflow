c994f5d6a7a30737ba6f3476abb267f066e2a01d
Erick Rodriguez <erickrod54@gmail.com>
2025-02-01 21:30:00 -0500

    git-and-github-workflow - version 2.17 - styled.components > index - Features: Importing and exporting SubTitle styles

--- src/styled-components/styled.components.index.js
+++ src/styled-components/styled.components.index.js
@@ -1,11 +1,11 @@
-import { HomePageContainer, MainContent, MainTitle } from "./styled.components";
+import { HomePageContainer, MainContent, MainTitle, SubTitle } from "./styled.components";
 
-/**git-and-github-workflow  - version 1.10 - styled.components > index
+/**git-and-github-workflow  - version 2.17 - styled.components > index
  * - Features: 
  * 
- *     --> importing and Exporting 'MainTitle' styles    
+ *     --> importing and Exporting 'SubTitle' styles    
  * 
  * Note: this is the index components
  */
 
-export { HomePageContainer, MainContent, MainTitle }
+export { HomePageContainer, MainContent, MainTitle, SubTitle }
