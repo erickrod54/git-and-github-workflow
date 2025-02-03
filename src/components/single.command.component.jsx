2d287eade2bfa7db8992e12c08c6b38b6e7b6fa2
Erick Rodriguez <erickrod54@gmail.com>
2025-02-02 21:33:00 -0500

    git-and-github-workflow - version 2.20 - SingleCommand - Features: Migrating SingleCommnadContainer

--- src/components/single.command.component.jsx
+++ src/components/single.command.component.jsx
@@ -2,18 +2,15 @@ import React from "react"
 import BashCode from "./bash.code.component"
 import { useGitGuideContext } from "../context"
 import styled from "styled-components"
+import { SingleCommandContainer } from "../styled-components/styled.components"
 
-/**git-and-github-workflow  - version 2.12 - SingleCommand
+/**git-and-github-workflow  - version 2.20 - SingleCommand
  * - Features: 
  * 
- *     --> getting drilled commands prop
+ *     --> Migrating 'SingleCommandContainer'
  * 
  * Note: this is the index components
  */
-
-const SingleCommandContainer = styled.div`
-    margin: 1rem;
-`
 
 const SingleCommand = ({ command }) => {
     
