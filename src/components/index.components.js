39054ba62a43abb79a5b035392a5c068a1dc6395
Erick Rodriguez <erickrod54@gmail.com>
2025-02-04 03:13:51 -0500

    git-and-github-workflow - version 1.02 - components > index - Features: Importing and exporting CommandsPallete

--- src/components/index.components.js
+++ src/components/index.components.js
@@ -1,12 +1,13 @@
+import CommandsPallete from "./commands.palette.component";
 import FooterComponent from "./footer.component";
 import HeaderComponent from "./header.component";
 
-/**git-and-github-workflow  - version 1.01 - components > index
+/**git-and-github-workflow  - version 1.02 - components > index
  * - Features: 
  * 
- *     --> Importing and exporting 'FooterComponent'    
+ *     --> Importing and exporting 'CommandsPallete'    
  * 
  * Note: this is the index components
  */
 
-export { HeaderComponent, FooterComponent }
+export { HeaderComponent, FooterComponent, CommandsPallete }
