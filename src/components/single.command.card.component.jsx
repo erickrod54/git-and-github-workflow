2969ad89adc211b8231f04f59ec44f6abcbfae81
Erick Rodriguez <erickrod54@gmail.com>
2025-02-03 21:34:00 -0500

    git-and-github-workflow - version 3.02 - SingleCardCommand - Features: Importing BoxContent and Title styles

--- src/components/single.command.card.component.jsx
+++ src/components/single.command.card.component.jsx
@@ -2,12 +2,12 @@ import React from "react";
 import styled from "styled-components";
 import SingleCommand from "./single.command.component";
 import { useGitGuideContext } from "../context";
-import { BoxContainer, CardContainer } from "../styled-components/styled.components";
+import { BoxContainer, BoxContent, CardContainer, Title } from "../styled-components/styled.components";
 
-/**git-and-github-workflow  - version 3.01 - SingleCardCommand
+/**git-and-github-workflow  - version 3.02 - SingleCardCommand
  * - Features: 
  * 
- *     --> Migrating 'BoxContainer' styles 
+ *     --> Importing 'BoxContent' and 'Title' styles 
  *   
  * Note: this is the index components
  */
