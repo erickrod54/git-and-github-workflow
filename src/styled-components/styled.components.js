01e93f169be545e14173ca2d9ab6ace10f1d97f0
Erick Rodriguez <erickrod54@gmail.com>
2025-02-03 21:31:00 -0500

    git-and-github-workflow - version 3.01 - styledcomponent - Features: writting BoxContainer

--- src/styled-components/styled.components.js
+++ src/styled-components/styled.components.js
@@ -1,9 +1,9 @@
 import styled, { createGlobalStyle } from "styled-components"
 
-/**git-and-github-workflow  - version 3.00 - styledcomponent
+/**git-and-github-workflow  - version 3.01 - styledcomponent
  * - Features: 
  * 
- *     --> writting 'CardContainer'      
+ *     --> writting 'BoxContainer'      
  * 
  * Note: this is the index components
 */
@@ -14,6 +14,58 @@ const breakpoints = {
     tablet: "768px",
     desktop: "1024px",
   };
+
+export const BoxContainer = styled.div`
+
+    padding: 1rem;
+    background-color: #fab5704c  ;
+    border: 1px solid rgba(255, 255, 255, 0.222);
+    -webkit-backdrop-filter: blur(20px);
+    backdrop-filter: blur(20px);
+    border-radius: 0.7rem;
+    transition: all ease 0.3s;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    margin-bottom: 2rem;
+    
+
+    &:hover {
+        box-shadow: 0px 0px 20px 1px #ffbb763f;
+        border: 1px solid rgba(255, 255, 255, 0.454);
+    }
+    `;
+
+    export const Title = styled.div`
+    font-size: 2rem;
+    font-weight: 500;
+    letter-spacing: 0.1em;
+    margin-bottom: 1rem;
+    `;
+
+    export const BoxContent = styled.div`
+    strong {
+        display: block;
+        margin-bottom: 0.5rem;
+    }
+
+    p {
+        margin: 0;
+        font-size: 1.5em;
+        font-weight: 300;
+        letter-spacing: 0.1em;
+    }
+
+    span {
+        font-size: 0.7rem;
+        font-weight: 300;
+    }
+
+    span:nth-child(3) {
+        font-weight: 500;
+        margin-right: 0.2rem;
+    }
+`;
 
 export const CardContainer = styled.div`
     color: black;
