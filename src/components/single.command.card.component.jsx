6187f784310b430df52eb12b2e66d850f86b7810
Erick Rodriguez <erickrod54@gmail.com>
2025-02-05 21:35:00 -0500

    git-and-github-workflow - version 3.08 - SingleCardCommand - Features: Migrating FrecuencyTitle

--- src/components/single.command.card.component.jsx
+++ src/components/single.command.card.component.jsx
@@ -2,19 +2,16 @@ import React from "react";
 import styled from "styled-components";
 import SingleCommand from "./single.command.component";
 import { useGitGuideContext } from "../context";
-import { BoxContainer, BoxContent, CardContainer, DescriptionBox, StrongContainer, Title } from "../styled-components/styled.components";
+import { BoxContainer, BoxContent, CardContainer, DescriptionBox, FrecuencyTitle, StrongContainer, Title } from "../styled-components/styled.components";
 
-/**git-and-github-workflow  - version 3.07 - SingleCardCommand
+/**git-and-github-workflow  - version 3.08 - SingleCardCommand
  * - Features: 
  * 
- *     --> writting 'FrecuencyTitle' 
+ *     --> Migrating 'FrecuencyTitle' 
  *   
  * Note: this is the index components
  */
 
-const FrecuencyTitle = styled.span`
-
-`
 
 const SingleCardComponent = () => {
 
