99782f61f65625c0e955c3f7535ee7fbb879ceef
Erick Rodriguez <erickrod54@gmail.com>
2025-02-06 23:56:25 -0500

    git-and-github-workflow - version 1.10 - HeaderComponent - Features: Migrating MainTitle styles

--- src/components/header.component.jsx
+++ src/components/header.component.jsx
@@ -1,27 +1,16 @@
 import React from "react";
 import styled from "styled-components";
+import { MainTitle } from "../styled-components/styled.components";
 
-/**git-and-github-workflow  - version 1.09 - HeaderComponent
+/**git-and-github-workflow  - version 1.10 - HeaderComponent
  * - Features: 
  * 
- *     --> writting 'MainTitle' styles    
+ *     --> Migrating 'MainTitle' styles    
  * 
  * Note: this is the index components
  */
 
-const MainTitle = styled.h2`
-    background-color: white;
-    color: #1f2937; /* Dark gray text */
-    font-size: 2rem;
-    font-weight: 700;
-    text-align: center;
-    padding: 1rem 2rem;
-    margin: 1.5rem auto;
-    border-radius: 8px;
-    box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1); /* Subtle border shadow */
-    border-left: 5px solid #2563eb; /* Blue left border */
-    max-width: 80%;
-`;
+
 
 const HeaderComponent = () => {
 
