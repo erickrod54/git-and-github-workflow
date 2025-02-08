5a3f070af296d1e10b3bc9912da1ae21524450b4
Erick Rodriguez <erickrod54@gmail.com>
2025-02-07 21:35:00 -0500

    git-and-github-workflow - version 3.13 - BashCode - Features: Increasing explicitly font size for token.cdat, and token.funtion

--- src/components/bash.code.component.jsx
+++ src/components/bash.code.component.jsx
@@ -7,17 +7,22 @@ import { clipboard } from '../assets/index.assets';
 import styled from 'styled-components';
 
 
-/**git-and-github-workflow  - version 3.10 - BashCode
+/**git-and-github-workflow  - version 3.13 - BashCode
  * - Features: 
  * 
  *     --> Incresing explicitly font size for 
- *          'token.punctuation', token.string, and .token.entity  
+ *          'token.cdata', and 'token.function'  
  * 
- * Note: this is the index components
+ * Note: to modify font sizes inspect the element, 
+ * look for the font color and set a font size:
+ * 
+ *    token.function --> color:#f08d49
+ * 
+ * this way can be highly custom
  */
 
 const Okadia = styled.div`
- code[class*=language-],pre[class*=language-]{color:#ccc;background:0 0;font-family:Consolas,Monaco,'Andale Mono','Ubuntu Mono',monospace;font-size:3rem;text-align:left;white-space:pre;word-spacing:normal;word-break:normal;word-wrap:normal;line-height:1.5;-moz-tab-size:4;-o-tab-size:4;tab-size:4;-webkit-hyphens:none;-moz-hyphens:none;-ms-hyphens:none;hyphens:none}pre[class*=language-]{padding:1em;margin:.5em 0;overflow:auto}:not(pre)>code[class*=language-],pre[class*=language-]{background:#2d2d2d}:not(pre)>code[class*=language-]{padding:.1em;border-radius:.3em;white-space:normal}.token.block-comment,.token.cdata,.token.comment,.token.doctype,.token.prolog{color:#999}.token.punctuation{color:#ccc; font-size:3rem;}.token.attr-name,.token.deleted,.token.namespace,.token.tag{color:#e2777a}.token.function-name{color:#6196cc}.token.boolean,.token.function,.token.number{color:#f08d49}.token.class-name,.token.constant,.token.property,.token.symbol{color:#f8c555}.token.atrule,.token.builtin,.token.important,.token.keyword,.token.selector{color:#cc99cd}.token.attr-value,.token.char,.token.regex,.token.string,.token.variable{color:#7ec699; font-size:3rem;}.token.entity,.token.operator,.token.url{color:#67cdcc; font-size:3rem}.token.bold,.token.important{font-weight:700}.token.italic{font-style:italic}.token.entity{cursor:help}.token.inserted{color:green}
+ code[class*=language-],pre[class*=language-]{color:#ccc;background:0 0;font-family:Consolas,Monaco,'Andale Mono','Ubuntu Mono',monospace;font-size:3rem;text-align:left;white-space:pre;word-spacing:normal;word-break:normal;word-wrap:normal;line-height:1.5;-moz-tab-size:4;-o-tab-size:4;tab-size:4;-webkit-hyphens:none;-moz-hyphens:none;-ms-hyphens:none;hyphens:none}pre[class*=language-]{padding:1em;margin:.5em 0;overflow:auto}:not(pre)>code[class*=language-],pre[class*=language-]{background:#2d2d2d}:not(pre)>code[class*=language-]{padding:.1em;border-radius:.3em;white-space:normal}.token.block-comment,.token.cdata,.token.comment,.token.doctype,.token.prolog{color:#999; font-size:1rem}.token.punctuation{color:#ccc; font-size:3rem;}.token.attr-name,.token.deleted,.token.namespace,.token.tag{color:#e2777a}.token.function-name{color:#6196cc}.token.boolean,.token.function,.token.number{color:#f08d49; font-size:2.2rem}.token.class-name,.token.constant,.token.property,.token.symbol{color:#f8c555; font-size:2.2rem}.token.atrule,.token.builtin,.token.important,.token.keyword,.token.selector{color:#cc99cd}.token.attr-value,.token.char,.token.regex,.token.string,.token.variable{color:#7ec699; font-size:3rem;}.token.entity,.token.operator,.token.url{color:#67cdcc; font-size:3rem}.token.bold,.token.important{font-weight:700}.token.italic{font-style:italic}.token.entity{cursor:help}.token.inserted{color:green}
 `
 const BashCode = ({ code }) => {
     const codeRef = useRef(null);
