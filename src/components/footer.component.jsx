cbc93910994cb782f39c416b9af1d6d94136fb28
Erick Rodriguez <erickrod54@gmail.com>
2025-02-09 22:54:28 -0500

    git-and-github-workflow - version 3.18 - FooterComponent - Features: writting FooterColumn

--- src/components/footer.component.jsx
+++ src/components/footer.component.jsx
@@ -1,10 +1,10 @@
 import React from "react";
 import styled from "styled-components";
 
-/**git-and-github-workflow  - version 3.17 - FooterComponent
+/**git-and-github-workflow  - version 3.18 - FooterComponent
  * - Features: 
  * 
- *     --> Refactoring 'FooterContainer' colors   
+ *     --> writting 'FooterColumn'   
  * 
  * Note: this is the index components
  */
@@ -29,6 +29,38 @@ const FooterContainer = styled.footer`
     @media (max-width: 480px) {
     grid-template-columns: 1fr;
     text-align: left;
+    }
+`;
+
+const FooterColumn = styled.div`
+    h3 {
+        margin-bottom: 1rem;
+        font-size: 1.25rem;
+        color: #fbbf24; /* Yellow */
+    }
+
+    p {
+        font-size: 1rem;
+        color: #d1d5db; /* Light gray */
+    }
+
+    ul {
+        list-style: none;
+        padding: 0;
+    }
+
+    li {
+        margin-bottom: 0.5rem;
+    }
+
+    a {
+        text-decoration: none;
+        color: #93c5fd; /* Light blue */
+        transition: color 0.3s;
+
+        &:hover {
+            color: #60a5fa; /* Darker blue */
+        }
     }
 `;
 
