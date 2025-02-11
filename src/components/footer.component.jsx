82ded1abff184725bb0eb841867317dc58483edc
Erick Rodriguez <erickrod54@gmail.com>
2025-02-10 21:34:00 -0500

    git-and-github-workflow - version 3.20 - FooterComponent - Features: MIgrating FooterComponent

--- src/components/footer.component.jsx
+++ src/components/footer.component.jsx
@@ -1,36 +1,15 @@
 import React from "react";
 import styled from "styled-components";
+import { FooterContainer } from "../styled-components/styled.components";
 
-/**git-and-github-workflow  - version 3.19 - FooterComponent
+/**git-and-github-workflow  - version 3.20 - FooterComponent
  * - Features: 
  * 
- *     --> Refactoring 'FooterColumn'   
+ *     --> Migrating 'FooterContainer'   
  * 
  * Note: this is the index components
  */
 
-const FooterContainer = styled.footer`
-    display: grid;
-    grid-template-columns: repeat(3, 1fr);
-    gap: 1.5rem;
-    border: 2px solid #43628f;
-    border-radius: 12px;
-    color: white;
-    padding: 2rem;
-    text-align: center;
-    color: black;
-
-    /* Tablet */
-    @media (max-width: 768px) {
-    grid-template-columns: 1fr 1fr;
-    }
-
-    /* Phone */
-    @media (max-width: 480px) {
-    grid-template-columns: 1fr;
-    text-align: left;
-    }
-`;
 
 const FooterColumn = styled.div`
     h3 {
