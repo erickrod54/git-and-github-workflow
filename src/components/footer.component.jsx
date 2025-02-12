878209a9bf9298c30aee33c2e0def69c3741a130
Erick Rodriguez <erickrod54@gmail.com>
2025-02-11 21:32:00 -0500

    git-and-github-workflow - version 4.01 - FooterComponent - Features: Building IconContainer

--- src/components/footer.component.jsx
+++ src/components/footer.component.jsx
@@ -2,14 +2,30 @@ import React from "react";
 import styled from "styled-components";
 import { FooterColumn, FooterContainer } from "../styled-components/styled.components";
 
-/**git-and-github-workflow  - version 4.00 - FooterComponent
+/**git-and-github-workflow  - version 4.01 - FooterComponent
  * - Features: 
  * 
- *     --> Migrating 'FooterColumn'   
+ *     --> Building 'IconContainer'   
  * 
  * Note: this is the index components
  */
 
+const IconContainer = styled.div`
+    display: flex;
+    justify-content: center;
+    gap: 1rem;
+    margin-top: 1rem;
+
+    a {
+        color: white;
+        font-size: 1.5rem;
+        transition: color 0.3s;
+
+        &:hover {
+            color: #fbbf24; /* Yellow */
+        }
+    }
+`;
 
 const FooterComponent = () => {
 
