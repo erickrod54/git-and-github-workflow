eb7ba6d6bea07c9a9aa29eef70e9a984f2011e05
Erick Rodriguez <erickrod54@gmail.com>
2025-02-11 21:31:00 -0500

    git-and-github-workflow - version 4.00 - FooterComponent - Features: Migrating FooterColumn

--- src/components/footer.component.jsx
+++ src/components/footer.component.jsx
@@ -1,47 +1,14 @@
 import React from "react";
 import styled from "styled-components";
-import { FooterContainer } from "../styled-components/styled.components";
+import { FooterColumn, FooterContainer } from "../styled-components/styled.components";
 
-/**git-and-github-workflow  - version 3.20 - FooterComponent
+/**git-and-github-workflow  - version 4.00 - FooterComponent
  * - Features: 
  * 
- *     --> Migrating 'FooterContainer'   
+ *     --> Migrating 'FooterColumn'   
  * 
  * Note: this is the index components
  */
-
-
-const FooterColumn = styled.div`
-    h3 {
-        margin-bottom: 1rem;
-        font-size: 1.25rem;
-        color: #f4a261; /* Yellow */
-    }
-
-    p {
-        font-size: 1rem;
-        color: #4f6688; /* Light gray */
-    }
-
-    ul {
-        list-style: none;
-        padding: 0;
-    }
-
-    li {
-        margin-bottom: 0.5rem;
-    }
-
-    a {
-        text-decoration: none;
-        color: #93c5fd; /* Light blue */
-        transition: color 0.3s;
-
-        &:hover {
-            color: #60a5fa; /* Darker blue */
-        }
-    }
-`;
 
 
 const FooterComponent = () => {
