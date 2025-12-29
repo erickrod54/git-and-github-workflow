9aad56e4e28635346b6451423d321d83bb83f570
Erick Rodriguez <erickrod54@gmail.com>
2025-12-28 21:35:00 -0600

    git-and-github-workflow - version 5.11 - components > index - Features: Importing and exporting SearchBar

--- src/components/index.components.js
+++ src/components/index.components.js
@@ -1,15 +1,16 @@
 import CommandsPallete from "./commands.palette.component";
 import FooterComponent from "./footer.component";
 import HeaderComponent from "./header.component";
+import SearchBar from "./search.bar.component";
 import SingleCardComponent from "./single.command.card.component";
 import SingleCommand from "./single.command.component";
 
-/**git-and-github-workflow  - version 2.05 - components > index
+/**git-and-github-workflow  - version 5.11 - components > index
  * - Features: 
  * 
- *     --> Importing and exporting 'SingleCardComponent'    
+ *     --> Importing and exporting 'SearchBar'    
  * 
  * Note: this is the index components
  */
 
-export { HeaderComponent, FooterComponent, CommandsPallete, SingleCommand, SingleCardComponent }
+export { HeaderComponent, FooterComponent, CommandsPallete, SingleCommand, SingleCardComponent, SearchBar }
