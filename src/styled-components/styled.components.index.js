976257f2d7f538fff3334f7eb90cdf56af241692
Erick Rodriguez <erickrod54@gmail.com>
2025-12-28 21:32:00 -0600

    git-and-github-workflow - version 5.09 - styled.components > index - Features: Importing and exporting SearchIcon

--- src/styled-components/styled.components.index.js
+++ src/styled-components/styled.components.index.js
@@ -11,17 +11,19 @@ import { BoxContainer,
          SearchContainer,
          SearchWrapper,
          StyledInput,
+         SearchIcon,
          MainContent, MainTitle, SingleCommandContainer, StrongContainer, SubTitle } from "./styled.components";
 
-/**git-and-github-workflow  - version 5.08 - styled.components > index
+/**git-and-github-workflow  - version 5.09 - styled.components > index
  * - Features: 
  * 
- *     --> importing and Exporting 'StyledInput' styles    
+ *     --> importing and Exporting 'SearchIcon' styles    
  * 
  * Note: this is the index components
  */
 
 export { HomePageContainer,
+         SearchIcon,
          StyledInput,
          SearchWrapper,
          SearchContainer,
