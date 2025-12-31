60069626c023fb9fb84cfd429413619a513fcd9f
Erick Rodriguez <erickrod54@gmail.com>
2025-12-30 21:30:00 -0600

    git-and-github-workflow - version 5.13 - utils > index - Features: Importing adn exporting getFrequencyLabel

---
+++ src/utils/utils.index.js
@@ -0,0 +1,13 @@
+import { getFrequencyLabel } from "./utils";
+
+/**git-and-github-workflow  - version 5.13 - utils > index
+ * - Features: 
+ * 
+ *     --> Importing and exporting 'getFrequencyLabel'
+ *   
+ * Note: In order to implement the search 'filterCommands'
+ * makes a new array from 'gitCommands' and then show the 
+ * results from the 'value' entered by the user 
+ */
+
+export { getFrequencyLabel }
