b169fff62f9792973a253942ea9e3af88578e346
Erick Rodriguez <erickrod54@gmail.com>
2026-01-09 21:34:00 -0600

    git-and-github-workflow - version 8.01 - utils > index - Features: Importing and exporting handleCopyCommand

--- src/utils/utils.index.js
+++ src/utils/utils.index.js
@@ -1,13 +1,13 @@
-import { getFrequencyLabel } from "./utils";
+import { getFrequencyLabel, handleCopyCommand } from "./utils";
 
-/**git-and-github-workflow  - version 5.13 - utils > index
+/**git-and-github-workflow  - version 8.01 - utils > index
  * - Features: 
  * 
- *     --> Importing and exporting 'getFrequencyLabel'
+ *     --> Importing and exporting 'handleCopyCommand'
  *   
  * Note: In order to implement the search 'filterCommands'
  * makes a new array from 'gitCommands' and then show the 
  * results from the 'value' entered by the user 
  */
 
-export { getFrequencyLabel }
+export { getFrequencyLabel, handleCopyCommand }
