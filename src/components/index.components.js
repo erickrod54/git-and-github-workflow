3044c1c49042a94858ece0a5f09a443434226065
Erick Rodriguez <erickrod54@gmail.com>
2026-01-15 21:32:00 -0600

    git-and-github-workflow - version 9.09 - components > index - Features: Importing and exporting GitCommandCard

--- src/components/index.components.js
+++ src/components/index.components.js
@@ -1,17 +1,18 @@
 import CommandsPallete from "./commands.palette.component";
 import FooterComponent from "./footer.component";
+import GitCommandCard from "./git.command.card.component";
 import HeaderComponent from "./header.component";
 import HighlightedText from "./highlight.component";
 import SearchBar from "./search.bar.component";
 import SingleCardComponent from "./single.command.card.component";
 import SingleCommand from "./single.command.component";
 
-/**git-and-github-workflow  - version 6.05 - components > index
+/**git-and-github-workflow  - version 9.09 - components > index
  * - Features: 
  * 
- *     --> Importing and exporting 'HighlightedText'    
+ *     --> Importing and exporting 'GitCommandCard'    
  * 
  * Note: this is the index components
  */
 
-export { HeaderComponent, FooterComponent, CommandsPallete, SingleCommand, SingleCardComponent, SearchBar, HighlightedText }
+export { HeaderComponent, FooterComponent, CommandsPallete, SingleCommand, SingleCardComponent, SearchBar, HighlightedText, GitCommandCard }
